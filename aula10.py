a8524f0fdf8d8a2dedd6a002e2eea8ca0e44e668
alinne-creator <alinne.oliveira237@gmail.com>
2024-08-09 16:09:37 -0300

    Add files via upload

---
+++ aula10.py
@@ -0,0 +1,8 @@
+for i in range (5):
+    nome= input("digite o nome do remedio: ")
+    preco= float(input("digite o preco do remedio:"))
+
+if (1<(2,3,4,5)):
+    print(input("o medicamento mais barato é: {nome}"))
+
+
